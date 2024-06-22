be5d9ce956c38e24ca6be5124894191795941861
Caio Vieira <caiocesarav0208@gmail.com>
2024-06-22 17:36:49 -0300

    feat: adiciona cors e começa a projetar rota

--- src/backend/server.ts
+++ src/backend/server.ts
@@ -1,8 +1,15 @@
 import express from "express";
 import { createServer } from "node:http";
 import { Server } from "socket.io";
+import cors from "cors";
 
 const app = express();
+app.use(
+  cors({
+    origin: ["http://localhost:5173"],
+  }),
+);
+app.use(express.json());
 const httpServer = createServer(app);
 const io = new Server(httpServer, {
   path: "/messages/",
@@ -21,10 +28,44 @@ interface Banco {
 }
 
 const pseudoBanco: Banco[] = [];
-let id = 0;
+interface Usuario {
+  id: number;
+  nome: string;
+  sala: string;
+}
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
+const pseudoBancoUsuario: Usuario[] = [];
+let id = 0;
+let idUsuario = 0;
+
+app.post("/entrar", (req, res) => {
+  const { sala, nome } = req.body || {};
+
+  console.log("sala: ", sala);
+  console.log("nome: ", nome);
+
+  //TODO: fazer uma validação mais seria que isso
+  if (!sala || !nome) {
+    res.statusCode = 412;
+    res.send("Dados invalidos para entrar");
+  }
+
+  let usuario = pseudoBancoUsuario.find(
+    ({ nome: nomeUsuario, sala: salaUsuario }) =>
+      nomeUsuario === nome && salaUsuario === sala,
+  );
+
+  if (!usuario) {
+    usuario = {
+      id: idUsuario,
+      nome,
+      sala,
+    };
+    idUsuario += 1;
+    pseudoBancoUsuario.push(usuario);
+  }
+
+  return res.send(usuario);
 });
 
 io.on("connection", (socket) => {
